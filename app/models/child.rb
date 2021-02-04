792897866ebf1a54386e2061f4fb04117231d888
Lily <lilyhuan@andrew.cmu.edu>
2021-02-04 16:23:52 -0600

    scopes and methods added to child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,5 +2,12 @@ class Child < ApplicationRecord
     has_many :chores
     has_many :tasks, through: :chores
 
+    scope :alphabetical, -> { order('last_name, first_name') }
+    scope :active, -> { where(active: true) }
+    
     validates_presence_of :first_name, :last_name
+
+    def name
+    	first_name + " " + last_name
+    end
 end
