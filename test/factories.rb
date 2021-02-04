607035047c7cd6296a2bf21e7bd1cc03bb322e62
Lily <lilyhuan@andrew.cmu.edu>
2021-02-04 16:20:27 -0600

    added validation and relationship to child model

--- test/factories.rb
+++ test/factories.rb
@@ -11,16 +11,16 @@ FactoryBot.define do
 
   # factory blueprint for tasks
   factory :task do
-    name "Wash dishes"
-    points 1
-    active true
+    name { "Wash dishes" }
+    points { 1 }
+    active { true }
   end
 
   # factory blueprint for chores
   factory :chore do
     association :child
     association :task
-    due_on 1.day.from_now.to_date
-    completed false
+    due_on { 1.day.from_now.to_date }
+    completed { false }
   end
 end
