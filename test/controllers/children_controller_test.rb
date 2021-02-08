e0648d0c777678108815ab487a20c87aa7adff97
Lily <lilyhuan@andrew.cmu.edu>
2021-02-07 19:38:35 -0600

    edited children_controller tests to pass

--- test/controllers/children_controller_test.rb
+++ test/controllers/children_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ChildrenControllerTest < ActionController::TestCase
   setup do
-    @child = children(:one)
+    @child = FactoryBot.create(:child)
   end
 
   test "should get index" do
@@ -18,30 +18,30 @@ class ChildrenControllerTest < ActionController::TestCase
 
   test "should create child" do
     assert_difference('Child.count') do
-      post :create, child: { active: @child.active, first_name: @child.first_name, last_name: @child.last_name }
+      post :create, params: { child: { active: @child.active, first_name: @child.first_name, last_name: @child.last_name } }
     end
 
     assert_redirected_to child_path(assigns(:child))
   end
 
   test "should show child" do
-    get :show, id: @child
+    get :show, params: { id: @child }
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @child
+    get :edit, params: { id: @child }
     assert_response :success
   end
 
   test "should update child" do
-    patch :update, id: @child, child: { active: @child.active, first_name: @child.first_name, last_name: @child.last_name }
+    patch :update, params: { id: @child, child: { active: @child.active, first_name: @child.first_name, last_name: @child.last_name } }
     assert_redirected_to child_path(assigns(:child))
   end
 
   test "should destroy child" do
     assert_difference('Child.count', -1) do
-      delete :destroy, id: @child
+      delete :destroy, params: { id: @child }
     end
 
     assert_redirected_to children_path
